7cb947f0e9da17aee03076b9f288b6776c5f8445
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 11:17:57 -0700

    exercise 10 done

--- ex10.js
+++ ex10.js
@@ -2,4 +2,19 @@
 // with words separated by spaces or underscores, and returns the string in camelCase format.
 // Example: convertToCamelCase("hello_world") should return "helloWorld".
 
+function convertToCamelCase(word) {
+  let result = [];
+
+  const split = word.indexOf("_") > 0 ? word.split("_") : word.split(" ");
+
+  for (let i = 1; i < split.length; i++) {
+    result.push(split[i][0].toUpperCase() + split[i].slice(1));
+  }
+
+  const joinResult = result.join("");
+
+  return `${split[0]}${joinResult}`;
+}
+
 console.log(convertToCamelCase("hello_world")); // Expected output: "helloWorld"
+// console.log(convertToCamelCase("hello word fer"));
