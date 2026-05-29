7daf5e015c679df5397e3aca355220119961d3cf
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 10:54:00 -0700

    exercise 7 done

--- ex07.js
+++ ex07.js
@@ -3,5 +3,11 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+//email: some@something.anotherThing
+//moc.elpmaxe@tset
+function validateEmail(email) {
+  return email.includes("@") && email.includes(".com") ? true : false;
+}
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
