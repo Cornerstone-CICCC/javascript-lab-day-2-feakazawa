826b59bc3c7df6383888e5d93f8dfa83cd5041a1
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 09:01:26 -0700

    exercise 1 done

--- ex01.js
+++ ex01.js
@@ -2,4 +2,15 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
+function capitalizeWords(word) {
+  let newWorld = "";
+
+  splitWord = word.split(" ");
+  splitWord.forEach((word) => {
+    newWorld = newWorld + word[0].toUpperCase() + word.slice(1) + " ";
+  });
+
+  return newWorld;
+}
+
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
