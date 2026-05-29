f89a974eadc6ff9a2717c49478edee14c5f1166b
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 09:48:39 -0700

    exercise 6 done

--- ex06.js
+++ ex06.js
@@ -3,4 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+function getQueryParams(url) {
+  let paramTemplate = { search: "", page: "" };
+
+  const initialSearchIdx = url.indexOf("=");
+  const finalSearchIdx = url.indexOf("&");
+  const resultSearch = url.slice(initialSearchIdx + 1, finalSearchIdx);
+
+  const initialPageIdx = url.indexOf("=", initialSearchIdx + 1);
+  const resultPage = url.slice(initialPageIdx + 1);
+
+  return {
+    ...paramTemplate,
+    search: resultSearch,
+    page: resultPage,
+  };
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
