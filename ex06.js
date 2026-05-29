1fdc362c2bc1c6178fce1494563f510cae40755e
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 10:34:26 -0700

    update solution ex 6

--- ex06.js
+++ ex06.js
@@ -5,19 +5,32 @@
 
 function getQueryParams(url) {
   let paramTemplate = { search: "", page: "" };
+  let result = [];
 
-  const initialSearchIdx = url.indexOf("=");
-  const finalSearchIdx = url.indexOf("&");
-  const resultSearch = url.slice(initialSearchIdx + 1, finalSearchIdx);
+  const splitUrl = url.split("=");
 
-  const initialPageIdx = url.indexOf("=", initialSearchIdx + 1);
-  const resultPage = url.slice(initialPageIdx + 1);
+  for (let i = 1; i < splitUrl.length; i++) {
+    let finalSearchIdx = splitUrl[i].indexOf("&");
+    let word = splitUrl[i].slice(0, finalSearchIdx);
+
+    if (i === splitUrl.length - 1) {
+      word = splitUrl[i].slice(0, splitUrl[i].length + 1);
+      result.push(word);
+    } else {
+      result.push(word);
+    }
+  }
 
   return {
     ...paramTemplate,
-    search: resultSearch,
-    page: resultPage,
+    search: result[0],
+    page: result[1],
   };
 }
 
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
+// console.log(
+//   getQueryParams(
+//     "https://example.com?search=test&page=2&message=hello&name=Fernanda",
+//   ),
+// ); // Expected output: { search: "test", page: "2" }
