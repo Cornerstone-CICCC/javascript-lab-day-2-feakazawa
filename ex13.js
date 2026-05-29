88bebd78a5eebc63f2d8c388a3a5d3cd699eb03a
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 11:31:56 -0700

    exercise 13 done

--- ex13.js
+++ ex13.js
@@ -2,4 +2,12 @@
 // and returns the sum of all numbers from 1 to `n`.
 // Example: sumRange(5) should return 15 (1 + 2 + 3 + 4 + 5).
 
+function sumRange(n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n + sumRange(n - 1);
+  }
+}
+
 console.log(sumRange(5)); // Expected output: 15
