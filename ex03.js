f80d1fc75c596eed2f6e824d1bd3850175977f7c
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 09:23:47 -0700

    exercise 3 done

--- ex03.js
+++ ex03.js
@@ -2,4 +2,13 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+function findMissingNumber(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== 1) {
+      missingValue = arr[i] - 1;
+    }
+  }
+  return missingValue;
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
