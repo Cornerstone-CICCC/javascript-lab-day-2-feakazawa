8271bd39b1b42e58bbdaac1a84bcdffadf1c9cb7
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-29 08:51:02 -0700

    thanks teacher - ex 4 done

--- ex04.js
+++ ex04.js
@@ -2,4 +2,29 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
-console.log(mostFrequentChar("javascript")); // Expected output: "a"
+// Expected output: "a"
+
+function mostFrequentChar(word) {
+  const characters = {};
+
+  for (const char of word) {
+    if (!characters[char]) {
+      characters[char] = 0;
+    }
+    characters[char] += 1;
+  }
+
+  let mostFrequent = ""; //stores the most frequent char
+  let currMax = 0; //stores the number of times it appears
+
+  for (const char of word) {
+    if (characters[char] > currMax) {
+      currMax = characters[char];
+      mostFrequent = char;
+    }
+  }
+
+  return mostFrequent;
+}
+
+console.log(mostFrequentChar("javascript"));
